934363ff7ce27b27b80f97f9a6c90d17c2f5f99a
Priyanshuraman5 <Priyanshuraman84@gmail.com>
2026-04-06 14:21:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -22,7 +22,7 @@ class Solution {
         dp[n] = 0;
         dp[n+1] = 0;
         for(int i=n-1;i>=0;i--){
-            dp[i] =   Math.min(dp[i+1]+cost[i],dp[i+2]+cost[i]);
+            dp[i] = cost[i] +  Math.min(dp[i+1],dp[i+2]);
         }
         return Math.min(dp[0],dp[1]);
     }
