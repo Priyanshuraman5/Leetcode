4b85fd29424a21776fde6cc356b28b04fbed9f29
Priyanshuraman5 <Priyanshuraman84@gmail.com>
2026-04-07 16:22:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -17,15 +17,31 @@
 //         return unique(m-1,n-1,dp);
 //     }
 // }
+// class Solution {
+//     public int uniquePaths(int m, int n){
+//         int[][] dp = new int[m+1][n+1];
+//         dp[0][1] = 1;
+//         for(int i=1;i<=m;i++){
+//             for(int j=1;j<=n;j++){
+//                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
+//             }
+//         }
+//         return dp[m][n];
+//     }
+// }
 class Solution {
     public int uniquePaths(int m, int n){
-        int[][] dp = new int[m+1][n+1];
-        dp[0][1] = 1;
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        int[]dp = new int[n];
+        dp[0] = 1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(m==0 && n==0) dp[j] = 1;
+                else if(j>0){
+                    dp[j] = dp[j]+dp[j-1];
+                }
             }
         }
-        return dp[m][n];
+        return dp[n-1];
     }
+
 }
