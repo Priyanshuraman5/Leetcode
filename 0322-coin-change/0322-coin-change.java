d4cafe7a4258314d02842c923bcd72cf4ed4080e
Priyanshuraman5 <Priyanshuraman84@gmail.com>
2026-05-11 12:25:17 +0530

    Time: 39 ms (8.89%), Space: 48.6 MB (5.13%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,27 +1,25 @@
 class Solution {
+    Integer[][] dp;
     public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
-        int[][] dp = new int[n][amount+1];
-        for(int col=0;col<=amount;col++){
-            if(col%coins[0]==0) {
-                dp[0][col] = col/coins[0];
-            }
-            else{
-                dp[0][col] = Integer.MAX_VALUE;
-            }
+        dp = new Integer[coins.length][amount+1];
+       
+        int ans = helper(coins,amount,0);
+        if(ans>= 400000) return -1;
+        return ans;
+    }
+    public int helper(int[] coins, int amount, int idx){
+        if(amount==0) return 0;
+
+        if(idx>=coins.length) return 400000;
+
+        if(dp[idx][amount]!=null) return dp[idx][amount];
+        int pick = Integer.MAX_VALUE;
+
+        if(amount>=coins[idx]){
+            pick = helper(coins,amount-coins[idx],idx) + 1;
         }
-        for(int row=1;row<n;row++){
-            for(int col=0;col<=amount;col++){
-                int exclude = dp[row-1][col];
-                int include = Integer.MAX_VALUE;
-                if(coins[row]<=col && dp[row][col-coins[row]] != Integer.MAX_VALUE){
-                    include = 1 + dp[row][col-coins[row]];
-                }
-                dp[row][col] = Math.min(include,exclude);
-                
-            }
-        }
-        if(dp[n-1][amount]==Integer.MAX_VALUE) return -1;
-        return dp[n-1][amount];
+        int skip = helper(coins,amount,idx+1);
+
+        return dp[idx][amount] = Math.min(pick,skip);
     }
 }
