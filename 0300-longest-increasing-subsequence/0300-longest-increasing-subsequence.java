4b0eaf66a8021c9cacfdf3643486aca7ec7f01cf
Priyanshuraman5 <Priyanshuraman84@gmail.com>
2026-05-12 10:18:06 +0530

    Time: 488 ms (5.02%), Space: 272.7 MB (6.8%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,18 +1,19 @@
 class Solution {
-    Integer[][] dp;
+    Integer [][]dp;
     public int lengthOfLIS(int[] nums) {
-        dp = new Integer[nums.length+1][nums.length+1];
+        dp = new Integer[nums.length][nums.length];
         return helper(nums,0,-1);
     }
-    public int helper(int[] nums, int idx,int prev){
-        if(idx==nums.length) return 0;
-        if(dp[prev+1][idx]!=null) return dp[prev+1][idx];
-        int pick =0;
-        if(prev==-1 || nums[idx]>nums[prev] ){
-            pick =  1+helper(nums,idx+1,idx);
+    public int helper(int[] nums, int idx, int prev){
+        int n = nums.length;
+        if(idx>=n) return 0;
+        if(dp[idx][prev+1] != null) return dp[idx][prev+1];
+        int pick = 0;
+
+        if(prev == -1 || nums[idx]>nums[prev] ){
+            pick = 1 + helper(nums,idx+1,idx);
         }
         int skip = helper(nums,idx+1,prev);
-        return dp[prev+1][idx] = Math.max(pick,skip);
-
+        return dp[idx][prev+1] = Math.max(pick,skip);
     }
 }
