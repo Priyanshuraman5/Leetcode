75600911bccff64a6fc56bc4d86eaa3b3607127f
Priyanshuraman5 <Priyanshuraman84@gmail.com>
2026-07-11 15:37:23 +0530

    Time: 23 ms (93.74%), Space: 49.1 MB (22.23%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,17 +1,35 @@
 class Solution {
-    public int subarraySum(int[] nums, int k) {
-        // Map<Integer,Integer> map = new HashMap<>();
-        int n = nums.length;
+    public int subarraySum(int[] arr, int k) {
+        // // Map<Integer,Integer> map = new HashMap<>();
+        // int n = nums.length;
+        // // for(int i=0;i<n;i++){
+        // //     map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        // // }
+        // int count = 0;
         // for(int i=0;i<n;i++){
-        //     map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        //     int sum = 0;
+        //     for(int j=i;j<n;j++){
+        //         sum += nums[j];
+        //         if(sum==k) count++;
+        //     }
         // }
+        // return count;
+        int n = arr.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int sum = 0;
         int count = 0;
+        map.put(0,1);
+        
         for(int i=0;i<n;i++){
-            int sum = 0;
-            for(int j=i;j<n;j++){
-                sum += nums[j];
-                if(sum==k) count++;
+            sum += arr[i];
+            
+            int delete = sum - k;
+            
+            if(map.containsKey(delete)){
+                count  = count + map.get(delete);
             }
+            
+            map.put(sum,map.getOrDefault(sum,0)+1);
         }
         return count;
     }
