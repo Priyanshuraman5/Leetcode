fc45efab32036a1d6090761337548165c2548112
Priyanshuraman5 <Priyanshuraman84@gmail.com>
2026-07-13 02:21:26 +0530

    Time: 0 ms (100%), Space: 43.5 MB (46.15%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,57 +1,19 @@
-// class Solution {    
-//     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-//         int m = obstacleGrid.length;
-//         int n = obstacleGrid[0].length;
-//         dp = new int[m][n];
-//         for(int[] row:dp){
-//             Arrays.fill(row,-1);
-//         }
-//         return helper(obstacleGrid,0,0);
-//     }
-//     public int helper(int[][]obstacleGrid,int i,int j){
-//         int m = obstacleGrid.length;
-//         int n = obstacleGrid[0].length;
-//         if(i>=m || j>=n) return 0;
-//         if(obstacleGrid[i][j]==1) return 0;
-//         if(i==m-1 && j==n-1) return 1;
-//         if(dp[i][j]!=-1) return dp[i][j];
-//         int right = helper(obstacleGrid,i+1,j);
-//         int down = helper(obstacleGrid,i,j+1);
-//         return dp[i][j] = right+down;
-//     }
-//     int[][]dp;
-// }
-// class Solution {    
-//     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-//         int m = obstacleGrid.length;
-//         int n = obstacleGrid[0].length;
-//         int[][] dp = new int[m+1][n+1];
-//         dp[0][1] =1;
-//         for(int i=1;i<=m;i++){
-//             for(int j=1;j<=n;j++){
-//                 if(obstacleGrid[i-1][j-1]==1) dp[i][j] = 0;
-//                 else{
-//                     dp[i][j] = dp[i-1][j] + dp[i][j-1];
-//                 }
-//             }
-//         }
-//         return dp[m][n];
-//     }
-// }
-class Solution {    
+class Solution {
+    Integer[][] dp;
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
-        int[] dp = new int[n];
-        dp[0] = 1;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(obstacleGrid[i][j]==1) dp[j] = 0;
-                else if(j>0){
-                    dp[j] = dp[j] + dp[j-1];
-                }
-            }
-        }
-        return dp[n-1];
+        dp = new Integer[m][n];
+        return helper(obstacleGrid,m,n,0,0);
+    }
+    public int helper(int[][]obstacleGrid, int m, int n, int i, int j){
+        if((i>=m || j>=n )||obstacleGrid[i][j]==1) return 0;
+        if(i==m-1 && j==n-1) return 1;
+        if(obstacleGrid[i][j]==1) return 0;
+        if(dp[i][j]!=null) return dp[i][j];
+        int pick1 = helper(obstacleGrid,m,n,i+1,j);
+        int pick2 = helper(obstacleGrid,m,n,i,j+1);
+
+        return dp[i][j] = pick1 + pick2;
     }
 }
