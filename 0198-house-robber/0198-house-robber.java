5f59c495500bbaa5006815c9bbaac8bbb1a8d546
Priyanshuraman5 <Priyanshuraman84@gmail.com>
2026-07-13 01:52:13 +0530

    Time: 0 ms (100%), Space: 43 MB (5.34%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,28 +1,16 @@
-// class Solution {
-//     int dp[];
-//     public int rob(int[] nums) {
-//         int n = nums.length;
-//         dp = new int[n+1];
-//         Arrays.fill(dp,-1);
-//         return helper(nums,0,n);
-//     }
-//     public int helper(int[] nums,int idx,int n){
-//         if(idx==n) return 0;
-//         if(idx>n)  return 0;
-//         if(dp[idx]!=-1) return dp[idx];
-//         int take = helper(nums,idx+2,n)+nums[idx];
-//         int skip = helper(nums,idx+1,n);
-//         return dp[idx]=Math.max(take, skip);
-//     }
-// }
 class Solution {
-    
+    Integer[] dp;
     public int rob(int[] nums) {
-        int n = nums.length;
-        int[]dp = new int[n+2];
-        for(int i=n-1;i>=0;i--){
-            dp[i] = Math.max(dp[i+1],dp[i+2]+nums[i]);
-        }
-        return dp[0];
+        dp = new Integer[nums.length];
+        return Math.max(helper(nums,0),helper(nums,1));
+
+    }
+
+    public int helper(int[] nums,int idx){
+        if(idx>=nums.length) return 0;
+        if(dp[idx]!=null) return dp[idx];
+        int pick = helper(nums,idx+2) + nums[idx];
+        int skip = helper(nums,idx+1);
+        return dp[idx] = Math.max(pick,skip);
     }
 }
