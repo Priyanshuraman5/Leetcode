5ea3bf28ae528169ea6f1127120cad345cd0278d
Priyanshuraman5 <Priyanshuraman84@gmail.com>
2026-07-13 17:45:21 +0530

    Time: 39 ms (8.56%), Space: 49 MB (5.36%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,50 +1,26 @@
-// class Solution {
-//     Integer[][] dp;
-//     public int coinChange(int[] coins, int amount) {
-//         dp = new Integer[coins.length][amount+1];
-       
-//         int ans = helper(coins,amount,0);
-//         if(ans>= 400000) return -1;
-//         return ans;
-//     }
-//     public int helper(int[] coins, int amount, int idx){
-//         if(amount==0) return 0;
-
-//         if(idx>=coins.length) return 400000;
-
-//         if(dp[idx][amount]!=null) return dp[idx][amount];
-//         int pick = Integer.MAX_VALUE;
-
-//         if(amount>=coins[idx]){
-//             pick = helper(coins,amount-coins[idx],idx) + 1;
-//         }
-//         int skip = helper(coins,amount,idx+1);
-
-//         return dp[idx][amount] = Math.min(pick,skip);
-//     }
-// }
-
-class Solution{
-    public int coinChange(int[] coins, int amount){
+class Solution {
+    Integer[][] dp;
+    public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int[][] dp = new int[n+1][amount+1];
-        
-        for(int i=1;i<=amount;i++){
-            dp[0][i] = 400000;
+        dp = new Integer[n][amount+1];
+        int ans = helper(coins,amount,0);
+        return ans==Integer.MAX_VALUE ? -1:ans;
+    }
+    public int helper(int[]coins, int amount, int idx){
+        int n = coins.length;
+        if(amount==0){
+            return 0;
         }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=amount;j++){
-                int skip = dp[i-1][j];
-                int pick = 400000;
-                if(j>=coins[i-1]){
-                    pick = 1 + dp[i][j-coins[i-1]];
-                }
-                dp[i][j] = Math.min(pick,skip);
+        if(idx==n) return Integer.MAX_VALUE;
+        if(dp[idx][amount]!=null) return dp[idx][amount];
+        int skip = helper(coins,amount,idx+1);
+        int pick = Integer.MAX_VALUE;
+        if(amount>=coins[idx]){
+            int store =  helper(coins,amount-coins[idx],idx);
+            if(store!=Integer.MAX_VALUE){
+                pick = 1 + store;
             }
         }
-        if(dp[n][amount]>=400000){
-            return -1;
-        }
-        return dp[n][amount];
+        return dp[idx][amount] = Math.min(pick,skip);
     }
 }
