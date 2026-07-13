05002193c3e6f5b3520c29f6a7286a103f7d07cb
Priyanshuraman5 <Priyanshuraman84@gmail.com>
2026-07-14 02:47:42 +0530

    Time: 3 ms (98.37%), Space: 47 MB (84.14%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,28 +1,21 @@
 class Solution {
     Integer[][] dp;
     public int minDistance(String word1, String word2) {
-        int m = word1.length();
-        int n = word2.length();
-        dp = new Integer[m][n];
-        return helper(word1, word2, 0, 0);
+        dp = new Integer[word1.length()+1][word2.length()+1];
+        return helper(word1,word2,word1.length(),word2.length());
     }
-    public int helper(String word1, String word2, int idx1, int idx2){
-        int m = word1.length();
-        int n = word2.length();
+    public int helper(String s1, String s2, int i, int j){
+        if(i==0) return j;
+        if(j==0) return i;
+        if(dp[i][j] != null) return dp[i][j];
+        if(s1.charAt(i-1)==s2.charAt(j-1)) return dp[i][j] = helper(s1,s2,i-1,j-1);
 
-        if(idx1>=m) return n - idx2;
-        if(idx2>=n) return m - idx1;
-
-        if(dp[idx1][idx2]!=null) return dp[idx1][idx2];
-
-        if(word1.charAt(idx1) == word2.charAt(idx2)){
-            return helper(word1, word2, idx1+1, idx2+1);
-        }
         else{
-            int insert =  helper(word1, word2, idx1, idx2+1);
-            int delete = helper(word1, word2, idx1+1, idx2);
-            int replace =  helper(word1, word2, idx1+1, idx2+1);
-            return dp[idx1][idx2] = 1 + Math.min(replace,Math.min(delete,insert));
+            int insert = 1+ helper(s1,s2,i,j-1);
+            int delete = 1+ helper(s1,s2,i-1,j);
+            int replace = 1+ helper(s1,s2,i-1,j-1);
+
+            return dp[i][j] = Math.min(replace,Math.min(insert,delete));
         }
     }
 }
