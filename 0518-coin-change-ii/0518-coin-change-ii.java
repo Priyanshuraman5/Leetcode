e798f7ed1fc1f007d1bcfd814b6b5225b9fa676b
Priyanshuraman5 <Priyanshuraman84@gmail.com>
2026-07-13 17:23:47 +0530

    Time: 30 ms (14.16%), Space: 55.1 MB (55.6%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,21 +1,21 @@
 class Solution {
     Integer[][] dp;
     public int change(int amount, int[] coins) {
-        int n = coins.length;
+        int n  =  coins.length;
         dp = new Integer[n][amount+1];
-        return helper(amount,coins,0); 
+        return helper(amount,coins, 0);
     }
     public int helper(int amount, int[] coins, int idx){
-
-        if(amount == 0) return 1;
+        if(amount==0) return 1;
         if(idx>=coins.length) return 0;
         if(dp[idx][amount]!=null) return dp[idx][amount];
+        int skip = helper(amount,coins,idx+1);
         int pick = 0;
         if(amount>=coins[idx]){
-            pick = helper(amount-coins[idx],coins,idx)  ;
+            pick =  helper(amount-coins[idx],coins,idx);
         }
-        int skip = helper(amount,coins,idx+1);
 
-        return dp[idx][amount] = pick + skip;
+        return dp[idx][amount] = pick+skip;
     }
+
 }
