c45f481b8821d87c2be275d6fab3815ff8322cc6
Priyanshuraman5 <Priyanshuraman84@gmail.com>
2026-07-15 15:43:54 +0530

    Time: 7 ms (13.99%), Space: 54.6 MB (8.53%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,12 +1,4 @@
 class Solution {
-    class Pair{
-        int row;
-        int col;
-        Pair(int row, int col){
-            this.row = row;
-            this.col = col;
-        }
-    }
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
@@ -14,7 +6,7 @@ class Solution {
         boolean[][] isVis = new boolean[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j] == '1'&&!isVis[i][j]){
+                if(grid[i][j]=='1' && !isVis[i][j]){
                     bfs(grid,i,j,isVis);
                     count++;
                 }
@@ -22,24 +14,32 @@ class Solution {
         }
         return count;
     }
-    public void bfs(char[][] grid, int i, int j, boolean[][]isVis){
-        int m = grid.length;
-        int n = grid[0].length;
+    class Pair{
+        int row;
+        int col;
+        public Pair(int row, int col){
+            this.row = row;
+            this.col = col;
+        }
+    }
+    public void bfs(char[][] isConnected,int row, int col,boolean[][] isVis){
+        int m = isConnected.length;
+        int n = isConnected[0].length;
         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(i,j));
-        isVis[i][j] = true;
+        q.add(new Pair(row,col));
+        isVis[row][col] = true;
         while(!q.isEmpty()){
             Pair p = q.poll();
-            int pRow = p.row;
-            int pCol = p.col;
-            int[] remRow = {0,0,-1,1};
-            int[] remCol = {1,-1,0,0};
-            for(int k=0;k<4;k++){
-                int newRow = remRow[k] + pRow;
-                int newCol = remCol[k] + pCol;
-                if(newRow>=0 && newCol>=0 && newRow<m && newCol<n && grid[newRow][newCol]=='1' && !isVis[newRow][newCol]){
-                    isVis[newRow][newCol] = true;
+            int currRow = p.row;
+            int currCol = p.col;
+            int[] remRow = {1,-1,0,0};
+            int[] remCol = {0,0,-1,1};
+            for(int i=0;i<4;i++){
+                int newRow = remRow[i] + currRow;
+                int newCol = remCol[i] + currCol;
+                if(newRow>=0 && newCol>=0 && newRow<m && newCol<n && isConnected[newRow][newCol]=='1' && !isVis[newRow][newCol]){
                     q.add(new Pair(newRow,newCol));
+                    isVis[newRow][newCol] = true;
                 }
             }
         }
