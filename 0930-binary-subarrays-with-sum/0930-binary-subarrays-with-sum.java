2c89c383843ecf05fe8dcdc4538ae1378d6f1756
Priyanshuraman5 <Priyanshuraman84@gmail.com>
2026-07-19 20:24:08 +0530

    Time: 22 ms (42.65%), Space: 52.7 MB (13.22%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,25 +1,43 @@
 class Solution {
-    public int numSubarraysWithSum(int[] nums, int goal) {
+    // public int numSubarraysWithSum(int[] nums, int goal) {
         
-        return helper(nums,goal) - helper(nums,goal-1);
+    //     return helper(nums,goal) - helper(nums,goal-1);
 
-    }
-    public int helper(int[] nums,int goal){
-        if(goal<0) return 0;
+    // }
+    // public int helper(int[] nums,int goal){
+    //     if(goal<0) return 0;
+    //     int n = nums.length;
+    //     int j = 0;
+    //     int count = 0;
+    //     int sum = 0;
+    //     for(int i=0;i<n;i++){
+    //         sum = sum + nums[i];
+    //         while(sum>goal){
+    //             sum -= nums[j];
+    //             j++;
+    //         }
+    //         count += (i-j+1);
+    //     }
+
+    //     return count;
+
+    // }
+    public int numSubarraysWithSum(int[] nums, int goal){
         int n = nums.length;
-        int j = 0;
-        int count = 0;
         int sum = 0;
+        int count = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
         for(int i=0;i<n;i++){
-            sum = sum + nums[i];
-            while(sum>goal){
-                sum -= nums[j];
-                j++;
+
+            sum += nums[i];
+            
+            if(map.containsKey(sum-goal)){
+                count += map.get(sum-goal);
             }
-            count += (i-j+1);
+
+            map.put(sum,map.getOrDefault(sum,0)+1);
         }
-
         return count;
-
     }
 }
