65321d04e492e1694097bae1a7b82be6502cc05c
Priyanshuraman5 <Priyanshuraman84@gmail.com>
2026-07-23 22:23:45 +0530

    Time: 2 ms (98.64%), Space: 72.6 MB (13.22%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -1,28 +1,31 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-   
         int count5 = 0;
         int count10 = 0;
         int count20 = 0;
         int n = bills.length;
+
         for(int i=0;i<n;i++){
-            if(bills[i]==5) count5++;
+            if(bills[i]==5){
+                count5++;
+            }
             else if(bills[i]==10){
-                count10++;
+                if(count5==0) return false;
                 count5--;
+                count10++;
             }
             else{
-                if(count10==0){
-                    count5 -= 3;
-                }
-                else{
-                    count10--;
-                    count5--;
-                }
                 count20++;
-                
+                if(count10==0){
+                    if(count5<3) return false;
+                    else count5 = count5-3;
+                }
+                else if(count5==0) return false;
+                else{
+                    count5--;
+                    count10--;
+                }
             }
-            if(count5<0 || count10<0 || count20<0) return false;
         }
         return true;
     }
