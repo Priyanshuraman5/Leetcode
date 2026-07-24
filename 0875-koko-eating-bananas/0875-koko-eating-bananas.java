420e75c762b2a6785f6697cbc3c4c7dca732eb21
Priyanshuraman5 <Priyanshuraman84@gmail.com>
2026-07-24 18:25:27 +0530

    Time: 27 ms (11.15%), Space: 48.6 MB (6.41%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -5,21 +5,21 @@ class Solution {
         for(int i=1;i<n;i++){
             max = Math.max(piles[i],max);
         }
-        int sta = 1;
+        int start = 1;
         int end = max;
-        int ans = 0;
-        while(sta<=end){
-            int mid = sta + (end - sta)/2;
+        int ans = max;
+        while(start<=end){
+            int mid = start + (end - start)/2;
             int hours = 0;
             for(int i=0;i<n;i++){
-                hours +=  Math.ceil((double)piles[i] / mid);
+                hours += Math.ceil(((double)piles[i])/mid);
             }
             if(hours<=h){
                 ans = mid;
                 end = mid - 1;
             }
             else{
-                sta = mid + 1;
+                start = mid + 1;
             }
         }
         return ans;
