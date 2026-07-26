fb6185f2ab909b00b5a3c60bf733cb630cca0878
Priyanshuraman5 <Priyanshuraman84@gmail.com>
2026-07-26 13:11:35 +0530

    Time: 6 ms (20.69%), Space: 54.8 MB (5.61%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -2,8 +2,8 @@ class Solution {
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int count = 0;
         boolean[][] isVis = new boolean[m][n];
+        int count = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1' && !isVis[i][j]){
@@ -22,7 +22,7 @@ class Solution {
             this.col = col;
         }
     }
-    public void bfs(char[][] isConnected,int row, int col,boolean[][] isVis){
+    public void bfs(char[][] isConnected,int row,int col,boolean[][] isVis){
         int m = isConnected.length;
         int n = isConnected[0].length;
         Queue<Pair> q = new LinkedList<>();
@@ -32,16 +32,17 @@ class Solution {
             Pair p = q.poll();
             int currRow = p.row;
             int currCol = p.col;
-            int[] remRow = {1,-1,0,0};
-            int[] remCol = {0,0,-1,1};
+            int[] remRow = {1,0,-1,0};
+            int[] remCol = {0,-1,0,1};
             for(int i=0;i<4;i++){
                 int newRow = remRow[i] + currRow;
                 int newCol = remCol[i] + currCol;
-                if(newRow>=0 && newCol>=0 && newRow<m && newCol<n && isConnected[newRow][newCol]=='1' && !isVis[newRow][newCol]){
+                if(newRow>=0 && newCol>=0 && newRow<m && newCol<n && !isVis[newRow][newCol] && isConnected[newRow][newCol]=='1'){
                     q.add(new Pair(newRow,newCol));
                     isVis[newRow][newCol] = true;
                 }
             }
         }
+
     }
 }
