f4739741d7a769b4b0e078e528e393d09313f1fa
Priyanshuraman5 <Priyanshuraman84@gmail.com>
2026-07-26 20:44:32 +0530

    Time: 620 ms (24.41%), Space: 43 MB (87.73%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -4,15 +4,16 @@ class Solution {
         return helper(nums,target,0);
     }
     public int helper(int[] nums, int target, int idx){
-        
         if(idx==nums.length){
-            if(target==0) return 1;
+            if(target==0){
+                return 1;
+            }
             else return 0;
+            
         }
-        
-        int  plus = helper(nums,target+nums[idx],idx+1);
-        
-        int minus = helper(nums,target - nums[idx],idx+1);
-        return (plus + minus);
+        int plus = helper(nums,target+nums[idx],idx+1);
+        int minus = helper(nums,target-nums[idx],idx+1);
+
+        return plus + minus;
     }
 }
